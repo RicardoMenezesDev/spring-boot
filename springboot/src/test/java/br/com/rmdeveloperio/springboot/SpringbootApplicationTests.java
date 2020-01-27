d2a5b7cec33acf9579a05f4e57cde258a5680c38
Ricardo Vinicius Muniz Menezes <ricardodeveloper21br@gmail.com>
2020-01-27 20:41:03 -0300

    Start project.

---
+++ springboot/src/test/java/br/com/rmdeveloperio/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rmdeveloperio.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
